06e9a3cca3866babcd145073084687f5421e6159
Juan Simancas <dev.juansimancas@proton.me>
2026-08-26 21:22:45 -0500

    extend openapi documentation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Elysia } from "elysia";
-import { openapi } from "@elysia/openapi";
+import { openapi, fromTypes } from "@elysia/openapi";
 import { CONFIG } from "@/config";
 import { apiRoutes } from "@/presentation/api";
 
-const app = new Elysia().use(openapi()).use(apiRoutes).listen(CONFIG.port);
+const app = new Elysia()
+	.use(
+		openapi({
+			references: fromTypes(),
+            documentation: {
+                info: {
+                    title: "Expenses App Documentation",
+                    version: "1.0.0"
+                }
+            }
+		}),
+	)
+	.use(apiRoutes)
+	.listen(CONFIG.port);
 
 export type App = typeof app;
 
